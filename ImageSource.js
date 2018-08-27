27daf552a1c3b9025d057db56b5de617bbf31ee1
bogdan_scher <bsc@piogroup.com>
2018-08-27 11:26:12 +0300

    prev images

--- ImageSource.js
+++ ImageSource.js
@@ -4,3 +4,4 @@ import React from 'react';
  * This constant will be updated by Yeoman-generator in the HelmPHR-version
  */
 export const imageSource = null;
+export const vaccinationsPrevImage = null;
